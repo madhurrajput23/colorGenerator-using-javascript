1f1e64a5a9e1dd77c5e474d4717135ef3c213078
Madhur Kumar <105670693+madhurrajput23@users.noreply.github.com>
2022-12-27 17:46:08 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,17 @@
+const getColor = () => {
+    const randomNumber =Math.floor(Math.random() * 16777215);
+    // console.log(randomNumber);
+    const randomCode = "#" + randomNumber.toString(16);
+    // console.log(randomNumber,randomCode);
+    document.body.style.backgroundColor = randomCode;
+    document.getElementById("color-code").innerText = randomCode;
+
+    // navigator.clipboard.writeText(randomCode);
+}
+//event call
+document.getElementById('btn').addEventListener(
+    "click",
+    getColor
+)
+// initial call when page refresh
+getColor();
